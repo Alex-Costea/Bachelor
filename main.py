7d6af5bd930ebb1c3ac56622f4319af472d08596
Alex Costea <alex.costea2520@gmail.com>
2022-04-02 18:52:04 +0200

    work in progress network discovery

--- main.py
+++ main.py
@@ -1,16 +1,87 @@
 import tweepy
 import networkx
+from collections import defaultdict
+import re
+import time
 
+#open the Twitter Client
 f = open("token.txt", "r")
 bearer_token=f.read()
+client = tweepy.Client(bearer_token,wait_on_rate_limit=True)
 
-client = tweepy.Client(bearer_token)
+#global variables
+requests=0 #number of tweets that have been alayzed
+requests_max=2000 #number of requests before quitting
+keywords=["onepiece","one piece","luffy"] 
+accounts_checked=set() #accounts that have already been analyzed
+pages=2 # 1 page = 100 tweets
+min_keywords=5 #smallest number of keywords for an account to be valid
+min_rts=2 #smallest number of retweets for a link
+sleep_time=1.1
 
-screen_name = "barackobama"
-user = client.get_user(username=screen_name)
-ID = user.data.id
+def get_id_from_username(name):
+    user = client.get_user(username=name)
+    time.sleep(sleep_time)
+    return user.data.id
 
-public_tweets = client.get_users_tweets(ID)
-for tweet in public_tweets.data:
-    print(tweet)
-    print("-------------------")
+def get_user_id_from_tweet(x):
+    time.sleep(sleep_time)
+    return client.get_tweet(x["id"],expansions=["author_id"]).includes["users"][0]["id"]
+
+def get_accounts_rted(ID,recursion_level=0):
+    global requests
+    if requests>=requests_max:
+        return
+    if recursion_level>100:
+        return
+    if ID in accounts_checked:
+        return
+
+    accounts_checked.add(ID)
+    keywords_found=0
+    
+    print("\n")
+    print("user ID",ID)
+
+    accounts_found=defaultdict(int)
+    public_tweets = tweepy.Paginator(
+                        client.get_users_tweets,
+                        ID,
+                        max_results=100,
+                        tweet_fields=["referenced_tweets"],
+                        limit=pages)
+    k=0
+    for page in public_tweets:
+        time.sleep(sleep_time)
+        for tweet in page.data:
+            tweet_text=tweet["text"]
+            k+=1
+            #find keywords
+            for key in keywords:
+                if re.search(key, tweet_text, re.IGNORECASE):
+                    keywords_found+=1
+            
+            requests+=1
+            referenced_tweets=tweet["referenced_tweets"]
+            if referenced_tweets:
+                for x in referenced_tweets:
+                    type_interaction=x["type"]
+                    if type_interaction=="retweeted":
+                        user_id=get_user_id_from_tweet(x)
+                        if user_id!=ID:
+                            accounts_found[user_id]+=1
+    print("tweets analyzed:",k)
+    if keywords_found>min_keywords:
+        print("keywords found:",keywords_found)
+        print("accounts found:")
+        for k,v in accounts_found.items():
+            if v>=min_rts:
+                print("ID:",k)
+                print("times:",v)
+        for k,v in accounts_found.items():
+            if v>=min_rts:
+                get_accounts_rted(k,recursion_level+1)
+    else:
+        print("false alarm!")
+        
+get_accounts_rted(get_id_from_username("Koishi_D_Sama"))
